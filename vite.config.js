94149cfd8c0eb3fec9a2b7289dba5dd0e240eb6e
Balint Kleri <b@balintbrews.com>
2025-12-02 11:32:25 +0100

    chore(vite.config): simplify module resolution aliases

--- vite.config.js
+++ vite.config.js
@@ -9,23 +9,9 @@ const DIR = path.dirname(fileURLToPath(import.meta.url));
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    // @todo Set up generic aliases here, and validate imports using ESLint.
-    alias: [
-      {
-        find: "@/lib/utils",
-        replacement: path.resolve(DIR, "./src/lib/utils.js"),
-      },
-      {
-        find: "@/lib/FormattedText",
-        replacement: path.resolve(DIR, "./src/lib/FormattedText.jsx"),
-      },
-      {
-        // Make sure we don't resolve nested folder structures under
-        // `@/components/`, like `@/components/atoms/button`.
-        // Those are not (yet) supported in Drupal Canvas.
-        find: /^@\/components\/([a-z_-]+)$/,
-        replacement: path.resolve(DIR, "./src/components/$1"),
-      },
-    ],
+    alias: {
+      "@/components": path.resolve(DIR, "./src/components"),
+      "@/lib": path.resolve(DIR, "./src/lib"),
+    },
   },
 });
